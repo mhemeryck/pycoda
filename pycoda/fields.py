a34c7511d0d4369a82035f6e35c01451d42c6722
Martijn Hemeryck <martijn.hemeryck@gmail.com>
2017-04-09 00:58:02 +0200

    Adds abstract base class and StringField
    The abstract base class describes how all Fields should look like.
    A field should have:
      - position: string position in the record
      - length: string width
      - value
      - tag: (optional, incurred from standard)
    
    The dumps method prints the value according the specs.
    
    The loads method reads a value according to the specs; it does by using 
    the parse method, which in turn calls it’s regex.
    
    The StringField is a first generic implementation.

--- pycoda/fields.py
+++ pycoda/fields.py
@@ -1,2 +1,58 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+
+import re
+from abc import ABCMeta, abstractmethod
+
+from six import with_metaclass
+
+
+class Field(with_metaclass(ABCMeta, object)):
+    def __init__(self, position, length, value=None, tag=None):
+        self.position = position
+        self.length = length
+        self.value = value
+        self.tag = tag
+
+    @abstractmethod
+    def _regex(self):
+        """Regular expression used in loading the value from a string"""
+
+    def _parse(self, string):
+        """Find match with regex and return if found"""
+        match = re.match(self._regex(), string)
+        if match is None:
+            raise ValueError('Specified string does not match field regex')
+        return match.group('value')
+
+    @abstractmethod
+    def dumps(self):
+        """Dump the value in the format such it can be picked up elsewhere"""
+
+    @abstractmethod
+    def loads(self, string):
+        """Parse value from given string, using the field's available regex"""
+
+
+class StringField(Field):
+    def __init__(self, position, length, value=None, tag=None,
+                 pad='', align='<'):
+        super(StringField, self).__init__(position, length,
+                                          value=value, tag=tag)
+        self.pad = pad
+        self.align = align
+
+    def _regex(self):
+        return r'^(?P<value>[\w\s]{{{self.length}}})$'.format(self=self)
+
+    def _parse(self, string):
+        return super(StringField, self)._parse(string)
+
+    def dumps(self):
+        value = self.value if self.value else ''
+        dump_format = '{value:{self.pad}{self.align}{self.length}s}'
+        return dump_format.format(self=self, value=value)[:self.length]
+
+    def loads(self, string):
+        self.value = self._parse(string)
+
--- pycoda/tests/test_fields.py
+++ pycoda/tests/test_fields.py
@@ -1,2 +1,131 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+
+from unittest import TestCase
+
+from pycoda.fields import (NumericField, StringField, ZeroesField, DateField, EmptyField)
+
+
+class StringFieldTest(TestCase):
+    def test_dumps(self):
+        field = StringField(0, 3, value='foo')
+        assert field.dumps() == 'foo'
+
+    def test_loads(self):
+        field = StringField(0, 3)
+        field.loads('foo')
+        assert field.value == 'foo'
+
+    def test_loads_dumps(self):
+        field = StringField(0, 3)
+        field.loads('foo')
+        assert field.dumps() == 'foo'
+
+    def test_loads_from_dumps(self):
+        field = StringField(0, 3, value='foo')
+        field.loads(field.dumps())
+        assert field.value == 'foo'
+
+    def test_new_field_from_dumps(self):
+        field = StringField(0, 3, value='foo')
+        new_field = StringField(0, 3, field.dumps())
+        assert new_field.value == 'foo'
+
+    def test_init_length_shorter_than_value(self):
+        field = StringField(0, 2, value='foo')
+        assert field.value == 'foo'
+
+    def test_init_length_longer_than_value(self):
+        field = StringField(0, 4, value='foo')
+        assert field.value == 'foo'
+
+    def test_init_empty_value(self):
+        field = StringField(0, 1, value=' ')
+        assert field.value == ' '
+
+    def test_init_leading_space(self):
+        field = StringField(0, 4, value=' foo')
+        assert field.value == ' foo'
+
+    def test_init_trailing_space(self):
+        field = StringField(0, 4, value='foo ')
+        assert field.value == 'foo '
+
+    def test_loads_length_shorter_than_value(self):
+        field = StringField(0, 2)
+        with self.assertRaises(ValueError):
+            field.loads('foo')
+
+    def test_loads_length_longer_than_value(self):
+        field = StringField(0, 4)
+        with self.assertRaises(ValueError):
+            field.loads('foo')
+
+    def test_loads_empty_string(self):
+        field = StringField(0, 1)
+        field.loads(' ')
+        assert field.value == ' '
+
+    def test_loads_leading_space(self):
+        field = StringField(0, 4)
+        field.loads(' foo')
+        assert field.value == ' foo'
+
+    def test_loads_trailing_space(self):
+        field = StringField(0, 4)
+        field.loads('foo ')
+        assert field.value == 'foo '
+
+    def test_loads_with_spaces(self):
+        field = StringField(0, 11)
+        field.loads('foo bar qux')
+        assert field.value == 'foo bar qux'
+
+    def test_dumps_length_shorter_than_value(self):
+        field = StringField(0, 2, value='foo')
+        assert field.dumps() == 'fo'
+
+    def test_dumps_length_longer_than_value(self):
+        field = StringField(0, 4, value='foo')
+        assert field.dumps() == 'foo '
+
+    def test_dumps_empty_string(self):
+        field = StringField(0, 1, value=' ')
+        assert field.dumps() == ' '
+
+    def test_dumps_empty_value(   self):
+        field = StringField(0, 3)
+        assert field.dumps() == '   '
+
+    def test_dumps_leading_space(self):
+        field = StringField(0, 4, value=' foo')
+        assert field.dumps() == ' foo'
+
+    def test_dumps_trailing_space(self):
+        field = StringField(0, 4, value= 'foo ')
+        assert field.dumps() == 'foo '
+
+    def test_dumps_init_none_value(self):
+        field = StringField(0, 3)
+        assert field.dumps() == '   '
+
+    def test_dumps_align_left(self):
+        field = StringField(0, 4, value='foo', pad=' ', align='<')
+        assert field.dumps() == 'foo '
+
+    def test_dumps_align_right(self):
+        field = StringField(0, 4, value='foo', pad=' ', align='>')
+        assert field.dumps() == ' foo'
+
+    def test_dumps_align_left_pad_zero(self):
+        field = StringField(0, 6, value='foo', pad='0', align='<')
+        assert field.dumps() == 'foo000'
+
+    def test_dumps_align_right_pad_zero(self):
+        field = StringField(0, 6, value='foo', pad='0', align='>')
+        assert field.dumps() == '000foo'
+
+    def test_dumps_pad_zero(self):
+        field = StringField(0, 3, pad=0)
+        assert field.dumps() == '000'
+
