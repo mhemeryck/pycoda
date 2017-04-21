bcac134c156c9dfa4302760ae2e56ffd972eb69f
Martijn Hemeryck <martijn.hemeryck@gmail.com>
2017-04-21 23:25:57 +0200

    Adds factories for Codafile + direct debit case
    Adds specific cases to all subfactories for direct debit case.

--- pycoda/factories.py
+++ pycoda/factories.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from datetime import date
+from datetime import date, datetime
+from decimal import Decimal
 
-from factory import Factory, fuzzy
+from factory import fuzzy, Factory, Trait, LazyFunction, LazyAttribute
 from factory.faker import Faker
 
+from pycoda.codafile import CodaFile
 from pycoda.records import (InitialRecord, OldBalanceRecord, TransactionRecord,
                             TransactionPurposeRecord, TransactionDetailRecord,
                             InformationRecord,
@@ -24,39 +26,116 @@ class InitialRecordFactory(Factory):
     addressee = Faker(b'name')
     bic = fuzzy.FuzzyChoice(('KREDBEBB', 'GKCCBEBB', 'BBRUBEBB'))
     account_holder_reference = fuzzy.FuzzyInteger(99999999999)
-    free = fuzzy.FuzzyText(length=5)
+    free = None
     transaction_reference = None
     related_reference = None
+
+    class Params:
+        direct_debit = Trait(
+            bank_identification_number=725,
+            reference='00417969',
+            addressee='UNLEASHED NV',
+            bic='KREDBEBB',
+            account_holder_reference=886946917,
+            free='00000'
+        )
 
 
 class OldBalanceRecordFactory(Factory):
     class Meta:
         model = OldBalanceRecord
 
+    old_balance = fuzzy.FuzzyDecimal(1e6, precision=3)
     balance_date = fuzzy.FuzzyDate(date(2000, 1, 1))
+
+    class Params:
+        direct_debit = Trait(
+            account_structure=2,
+            serial_number=256,
+            account_number='BE02737026917240                  EUR',
+            balance_sign=0,
+            account_holder_name='UNLEASHED NV',
+            account_description='Bedrijfsrekening',
+            bank_statement_serial_number=119,
+        )
 
 
 class TransactionRecordFactory(Factory):
     class Meta:
         model = TransactionRecord
 
+    serial_number = fuzzy.FuzzyInteger(1e4 - 1)
+    balance = fuzzy.FuzzyDecimal(1e6, precision=3)
     balance_date = fuzzy.FuzzyDate(date(2000, 1, 1))
     booking_date = fuzzy.FuzzyDate(date(2000, 1, 1))
+
+    class Params:
+        direct_debit = Trait(
+            detail_number=1,
+            bank_reference_number='AQQE12627 BHKDGLGTESC',
+            balance_sign=1,
+            balance=Decimal('15.000'),
+            balance_date=LazyFunction(datetime.now),
+            transaction_code=350000,
+            reference_type=0,
+            reference=None,
+            booking_date=LazyFunction(datetime.now),
+            bank_statement_serial_number=256,
+            globalisation_code=1,
+            transaction_sequence=True,
+            information_sequence=True
+        )
 
 
 class TransactionPurposeRecordFactory(Factory):
     class Meta:
         model = TransactionPurposeRecord
 
+    serial_number = fuzzy.FuzzyInteger(1e4 - 1)
+
+    class Params:
+        collect_file_id = 1234
+        direct_debit = Trait(
+            detail_number=2,
+            client_reference=LazyAttribute(
+                lambda o: 'C20160903040112-{o.collect_file_id}F'.format(o=o)),
+            transaction_sequence=True,
+            information_sequence=True,
+        )
+
 
 class TransactionDetailRecordFactory(Factory):
     class Meta:
         model = TransactionDetailRecord
 
+    serial_number = fuzzy.FuzzyInteger(1e3 - 1)
+    account_holder_name = Faker(b'name')
+
+    class Params:
+        payment_id = 1
+        direct_debit = Trait(
+            account_number='BE68539007547034',
+            description=LazyAttribute(
+                lambda o: '1-{o.payment_id}'.format(o=o)),
+            serial_number=22,
+        )
+
 
 class InformationRecordFactory(Factory):
     class Meta:
         model = InformationRecord
+
+    class Params:
+        direct_debit = Trait(
+            detail_number=4,
+            information_sequence=False,
+            transaction_sequence=False,
+            reference='001Martijn',
+            reference_number='CVYC01435BSCTOBAOVERS',
+            reference_type=1,
+            serial_number=22,
+            transaction_code=150000,
+        )
 
 
 class InformationPurposeRecordFactory(Factory):
@@ -84,3 +163,32 @@ class ExtraMessageRecordFactory(Factory):
 class FinalRecordFactory(Factory):
     class Meta:
         model = FinalRecord
+
+    credit = fuzzy.FuzzyDecimal(1e6, precision=3)
+    debit = fuzzy.FuzzyDecimal(1e6, precision=3)
+    number_records = fuzzy.FuzzyInteger(1e6-1)
+
+    class Params:
+        direct_debit = Trait(
+            has_sequence=False,
+        )
+
+
+class CodaFileFactory(Factory):
+    class Meta:
+        model = CodaFile
+
+    records = None
+
+    class Params:
+        direct_debit = Trait(
+            records=[
+                InitialRecordFactory(direct_debit=True),
+                OldBalanceRecordFactory(direct_debit=True),
+                TransactionRecordFactory(direct_debit=True),
+                TransactionPurposeRecordFactory(direct_debit=True),
+                TransactionDetailRecordFactory(direct_debit=True),
+                InformationRecordFactory(direct_debit=True),
+                FinalRecordFactory(direct_debit=True),
+            ],
+        )
