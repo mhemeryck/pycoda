a4f753828534aef0522b56ec8fb346e190b150be
Martijn Hemeryck <martijn.hemeryck@gmail.com>
2017-04-21 17:26:28 +0200

    Adds default record factories
    Adds working default factories for all record types.
    
    Additionally, also implemented the constructor for last records

--- pycoda/tests/test_factories.py
+++ pycoda/tests/test_factories.py
@@ -3,12 +3,121 @@ from __future__ import unicode_literals
 
 from unittest import TestCase
 
-from pycoda.factories import InitialRecordFactory
+from pycoda.factories import (InitialRecordFactory, OldBalanceRecordFactory,
+                              TransactionRecordFactory,
+                              TransactionPurposeRecordFactory,
+                              TransactionDetailRecordFactory,
+                              InformationRecordFactory,
+                              InformationPurposeRecordFactory,
+                              InformationDetailRecordFactory,
+                              NewBalanceRecordFactory,
+                              FinalRecordFactory,
+                              ExtraMessageRecordFactory)
 
 
 class InitialRecordFactoryTest(TestCase):
     def setUp(self):
         self.record = InitialRecordFactory()
+
+    def test_loads_from_dumps(self):
+        raw = self.record.dumps()
+        self.record.loads(raw)
+        assert self.record.dumps() == raw
+
+
+class OldBalanceRecordFactoryTest(TestCase):
+    def setUp(self):
+        self.record = OldBalanceRecordFactory()
+
+    def test_loads_from_dumps(self):
+        raw = self.record.dumps()
+        self.record.loads(raw)
+        assert self.record.dumps() == raw
+
+
+class TransactionRecordFactoryTest(TestCase):
+    def setUp(self):
+        self.record = TransactionRecordFactory()
+
+    def test_loads_from_dumps(self):
+        raw = self.record.dumps()
+        self.record.loads(raw)
+        assert self.record.dumps() == raw
+
+
+class TransactionPurposeRecordFactoryTest(TestCase):
+    def setUp(self):
+        self.record = TransactionPurposeRecordFactory()
+
+    def test_loads_from_dumps(self):
+        raw = self.record.dumps()
+        self.record.loads(raw)
+        assert self.record.dumps() == raw
+
+
+class TransactionDetailRecordFactoryTest(TestCase):
+    def setUp(self):
+        self.record = TransactionDetailRecordFactory()
+
+    def test_loads_from_dumps(self):
+        raw = self.record.dumps()
+        self.record.loads(raw)
+        assert self.record.dumps() == raw
+
+
+class InformationRecordFactoryTest(TestCase):
+    def setUp(self):
+        self.record = InformationRecordFactory()
+
+    def test_loads_from_dumps(self):
+        raw = self.record.dumps()
+        self.record.loads(raw)
+        assert self.record.dumps() == raw
+
+
+class InformationPurposeRecordFactoryTest(TestCase):
+    def setUp(self):
+        self.record = InformationPurposeRecordFactory()
+
+    def test_loads_from_dumps(self):
+        raw = self.record.dumps()
+        self.record.loads(raw)
+        assert self.record.dumps() == raw
+
+
+class InformationDetailRecordFactoryTest(TestCase):
+    def setUp(self):
+        self.record = InformationDetailRecordFactory()
+
+    def test_loads_from_dumps(self):
+        raw = self.record.dumps()
+        self.record.loads(raw)
+        assert self.record.dumps() == raw
+
+
+class NewBalanceRecordFactoryTest(TestCase):
+    def setUp(self):
+        self.record = NewBalanceRecordFactory()
+
+    def test_loads_from_dumps(self):
+        raw = self.record.dumps()
+        self.record.loads(raw)
+        assert self.record.dumps() == raw
+
+
+class ExtraMessageRecordFactoryTest(TestCase):
+    def setUp(self):
+        self.record = ExtraMessageRecordFactory()
+
+    def test_loads_from_dumps(self):
+        raw = self.record.dumps()
+        self.record.loads(raw)
+        assert self.record.dumps() == raw
+
+
+class FinalRecordFactoryTest(TestCase):
+    def setUp(self):
+        self.record = FinalRecordFactory()
 
     def test_loads_from_dumps(self):
         raw = self.record.dumps()
