047c4274702d4e6feb6923668627f1a79b5ebf6b
Martijn Hemeryck <martijn.hemeryck@gmail.com>
2019-03-20 21:17:28 +0100

    Adds alternate `codapy` name
    Other name was already in use on pypi.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ install_requires = ("factory-boy",)
 
 
 setup(
-    name="pycoda",
+    name="codapy",
     version=pycoda.__version__,
     description="Coded statement of Account (CODA) python API",
     url="https://github.com/mhemeryck/pycoda",
@@ -18,4 +18,11 @@ setup(
     license="MIT",
     packages=["pycoda"],
     zip_safe=True,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Office/Business :: Financial :: Accounting",
+    ],
 )
