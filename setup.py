cb73006c54acf8ae25fdca897c8eba01d2759921
Martijn Hemeryck <martijn.hemeryck@gmail.com>
2019-03-20 21:36:40 +0100

    Adds CI/CD setup for push to pypi

--- setup.py
+++ setup.py
@@ -1,16 +1,42 @@
 from __future__ import unicode_literals
 
+import os
+import sys
+
 from setuptools import setup
+from setuptools.command.install import install
 
 import pycoda
 
 install_requires = ("factory-boy",)
 
 
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our version"""
+
+    description = "verify that the git tag matches our version"
+
+    def run(self):
+        tag = os.getenv("CIRCLE_TAG")
+
+        if tag != pycoda.__version__:
+            info = "Git tag: {0} does not match the version of this app: {1}".format(
+                tag, pycoda.__version__
+            )
+            sys.exit(info)
+
+
+def readme():
+    """print long description"""
+    with open("README.rst") as f:
+        return f.read()
+
+
 setup(
     name="codapy",
     version=pycoda.__version__,
     description="Coded statement of Account (CODA) python API",
+    long_description=readme(),
     url="https://github.com/mhemeryck/pycoda",
     install_requires=install_requires,
     author="Martijn Hemeryck",
@@ -25,4 +51,5 @@ setup(
         "Programming Language :: Python :: 3",
         "Topic :: Office/Business :: Financial :: Accounting",
     ],
+    cmdclass={"verify": VerifyVersionCommand},
 )
